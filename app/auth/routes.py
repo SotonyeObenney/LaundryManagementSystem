fdb2ff13081c863b6667e40e9061fe9bfa6e9fbc
sotonye_obenney <obenneysotonye@gmail.com>
2026-04-13 13:47:04 +0100

    Add register.html and register route

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,13 @@
 from app.auth import auth_bp
+from flask import render_template   
+
+
+@auth_bp.route("/register")
+def register():
+    print(5)
+    return render_template("auth/register.html")
+
 
 @auth_bp.route("/login")
 def login():
-    return "Wagwan, login page coming soon"
+    return "Hello everyone I'm really tired!!!!"
