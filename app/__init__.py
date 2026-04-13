16324a6efd3fa33ffd2beb4bb48871858c876479
Auwal <mohammedauwalhassan07@gmail.com>
2026-04-13 20:43:45 +0100

    chore:update requirements.txt with installed libraries

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from config import Config
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='../templates')
     app.config.from_object(Config)
 
     # Initialize extensions
