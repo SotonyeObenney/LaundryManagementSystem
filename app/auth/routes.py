787d43e757fccd038fd18c76f2fd0297e9f07e99
sotonye_obenney <obenneysotonye@gmail.com>
2026-04-24 10:31:22 +0100

    Finish register WTF forms
    Simplify register.html
    
    removed the env/ and put .env files and .flaskenv in main app directory

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,23 @@
 from app.auth import auth_bp
-from flask import render_template   
+from flask import render_template, url_for, flash, redirect
+from .forms import RegistrationForm, LoginForm   
 
 
-@auth_bp.route("/register")
+@auth_bp.route("/register", methods=['GET', 'POST'])
 def register():
-    return render_template("auth/register.html")
+    form = RegistrationForm()
+
+    # This checks if the form was submitted AND if all validators passed
+    if form.validate_on_submit():
+        flash(f'Account created for {form.name.data}!', 'success')
+        # Here is where you would normally create your User object:
+        # new_user = User(name=form.name.data, email=form.email.data, ...)
+        # db.session.add(new_user)
+        # db.session.commit()
+        return redirect(url_for('auth_bp.login')) 
+    
+    return render_template('auth/register.html', form=form)
+    
 
 
 
