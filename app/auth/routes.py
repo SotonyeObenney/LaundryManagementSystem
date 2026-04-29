dc8af2027b7725499bbfd1b6115b6348bc7f4617
Auwal <mohammedauwalhassan07@gmail.com>
2026-04-29 16:50:42 +0100

    fix: login user after registration:

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,8 @@
 from ..auth import auth_bp
 from flask import render_template, url_for, flash, redirect
+from flask_login import login_user
 from .forms import RegistrationForm, LoginForm   
-from werkzeug.security import generate_password_hash
+from .. import bcrypt
 from ..models import User
 from ..extensions import db
 
@@ -9,13 +10,10 @@ from ..extensions import db
 def register():
     form = RegistrationForm()
 
-    # 1. THE FORM: Validate the form submission 
+    # 1. Validate the form submission 
     if form.validate_on_submit():
-        flash(f'Account created for {form.name.data}!', 'success')
-        # 2. THE TRANSFORMATION: Hash the password before saving it to the database 
-        hashed_password = generate_password_hash(form.password.data)
-        
-        # 3. THE MODEL: Create a new user instance with the form data and the hashed password
+        password = form.password.data
+        hashed_password = bcrypt.generate_password_hash(password=password).decode('utf-8') 
         new_user = User(
             name=form.name.data,
             email=form.email.data,
@@ -23,21 +21,17 @@ def register():
             address=form.address.data,
             password=hashed_password # Save the hash, NOT the raw password
         )
-        
-        # 4. THE DATABASE: Add the new user to the database and commit the transaction
         db.session.add(new_user)
         db.session.commit()
-        
-        # 5. THE FEEDBACK: Tell the user it worked and move them to login
-        flash('Registration successful! Please log in.', 'success')
-        return redirect(url_for('auth_bp.login'))
-    
+        login_user(new_user)
+        flash(f'Account created for {form.name.data}!', 'success') 
+        return "Account created successfully and user logged in. Redirecting to dashboard..."
     return render_template('auth/register.html', form=form)
-    
-
 
 
 @auth_bp.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        pass
     return render_template('auth/login.html', form=form)
