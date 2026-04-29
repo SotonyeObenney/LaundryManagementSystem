3906ab33930044501dc8bfd4df00103974334524
Auwal <mohammedauwalhassan07@gmail.com>
2026-04-29 16:54:40 +0100

    chore: add error messages to email validators and set minimum password length to 6 characters

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class RegistrationForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
-    email = StringField("Email", validators=[DataRequired(), Email()])
+    email = StringField("Email", validators=[DataRequired(), Email(message="Invalid email address")])
     phone = StringField("Phone", validators=[DataRequired()])
     address = StringField("Address", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password")])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6, message='Password must be at least 6 characters long')])
+    confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo("password"), Length(min=6)])
     # role  = StringField("Role", validators=[DataRequired()])
     # HTML code incase we want to add a dropdown for role selection in the future
     #  <div class="form-group">
